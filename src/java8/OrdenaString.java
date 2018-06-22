1ef0d02d36edd77b0a74d704696317543e46fdf0
rodrigoapolinaro <rodrigoapolinaro@icloud.com>
2018-06-22 02:04:18 -0300

    Aula02 Que venham os lambdas

--- src/java8/OrdenaString.java
+++ src/java8/OrdenaString.java
@@ -1,51 +1,34 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class OrdenaString {
 
 	public static void main(String[] args) {
-	
+		
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
-	
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		// Collections.sort(palavras, comparador);
-	
-		palavras.sort(comparador);
-	
-		// for (String p : palavras) {
-		// System.out.println(p);
-		// }
-	
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
-
+		
+		palavras.sort((s1, s2) -> {
+		    if (s1.length() < s2.length())
+		        return -1;
+		    if (s1.length() > s2.length())
+		        return 1;
+		    return 0;
+		});
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
 		System.out.println(palavras);
-	}
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
+		
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
+		
+		palavras.forEach(s -> System.out.println(s));
+	
 	}
 }
